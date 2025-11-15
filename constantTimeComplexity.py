d50ce532e61f1dae89ddd19b0f099ee545155ae3
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-11-15 16:01:29 +0500

    add example of constant time complexity O(1) and binary search O(log n)

---
+++ constantTimeComplexity.py
@@ -0,0 +1,8 @@
+def BigO(arr):
+    return arr[0]
+
+
+# Example usage:example_array = [42, 7, 19, 23, 5]
+result = BigO([42, 7, 19, 23, 5])
+print(result)  # Output: 42
+# The function BigO demonstrates constant time complexity O(1)
